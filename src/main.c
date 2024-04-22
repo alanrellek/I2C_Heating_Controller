3f1d11e41626f905419c29576ede2f3220d49251
Alan Keller <jedi@swissair.name>
2024-04-22 18:03:23 +0200

    added stuff

--- src/main.c
+++ src/main.c
@@ -7,24 +7,38 @@
 #include "driver/gpio.h"
 #include "driver/adc.h"
 
-#define LED_GPIO 2 // GPIO 2 is the built-in LED on the ESP32
-#define POWER_GPIO 12 // GPIO 12 is connected to the relay module
+#define LED_GPIO 2       // GPIO 2 is the built-in LED on the ESP32
+#define HEATER_GPIO 12   // GPIO 12 is connected to the relay module
 #define ADC1_CHANNEL_0 0 // GPIO 36 is the ADC pin for the thermistor
 
-#define UPPER_TEMP 40
-#define LOWER_TEMP 39
+#define TARGET_TEMP 35
 
-#define BUFFER_SIZE 100
-#define TEMP_DELTA_BUFFER_SIZE 100
+#define TEMP_BUFFER_SIZE 10
+#define TEMP_DELTA_BUFFER_SIZE 10
+#define TEMP_TARGET_DELTA_BUFFER_SIZE 10
+#define AV_TEMP_BUFFER_SIZE 10
 
-float temperatureBuffer[BUFFER_SIZE];
+float correction_power = 0.5;
+float control_cycle = 1;
+
+float temperatureBuffer[TEMP_BUFFER_SIZE];
 int currentTempBufferIndex = 0;
+
+float averageTemperatureBuffer[AV_TEMP_BUFFER_SIZE];
+int currentAverageTempBufferIndex = 0;
 
 float temperatureDeltaBuffer[TEMP_DELTA_BUFFER_SIZE];
 int currentTempDeltaBufferIndex = 0;
 
+float temperatureTargetDeltaBuffer[TEMP_TARGET_DELTA_BUFFER_SIZE];
+int currentTempTargetDeltaBufferIndex = 0;
+
 float readTemperature()
 {
+    // Configure ADC
+    adc1_config_width(ADC_WIDTH_BIT_12);                        // Configure ADC to 12-bit resolution
+    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11); // Configure attenuation for the pin
+
     // read the ADC value from the thermistor
     int adc_reading = adc1_get_raw(ADC1_CHANNEL_0);
     // printf("ADC Reading: %d\n", adc_reading);
@@ -40,7 +54,6 @@ float readTemperature()
     float tempKelvin = 1 / (1 / (273.15 + 25) + log(resistance / 10) / 3950.0);
     float tempCelsius = tempKelvin - 273.15;
 
-    // printf("Temperature: %f\n", tempCelsius);
     return tempCelsius;
 }
 
@@ -49,7 +62,31 @@ void updateTempBuffer(float temperature)
     // Store the temperature in the buffer
     temperatureBuffer[currentTempBufferIndex] = temperature;
     // Increment the index, wrapping around to the start of the buffer if necessary
-    currentTempBufferIndex = (currentTempBufferIndex + 1) % BUFFER_SIZE;
+    currentTempBufferIndex = (currentTempBufferIndex + 1) % TEMP_BUFFER_SIZE;
+}
+
+void updateAverageTempBuffer(float temperature)
+{
+    // Store the temperature in the buffer
+    averageTemperatureBuffer[currentAverageTempBufferIndex] = temperature;
+    // Increment the index, wrapping around to the start of the buffer if necessary
+    currentAverageTempBufferIndex = (currentAverageTempBufferIndex + 1) % AV_TEMP_BUFFER_SIZE;
+}
+
+void updateTempDeltaBuffer(float delta)
+{
+    // Store the temperature delta in the buffer
+    temperatureDeltaBuffer[currentTempDeltaBufferIndex] = delta;
+    // Increment the index, wrapping around to the start of the buffer if necessary
+    currentTempDeltaBufferIndex = (currentTempDeltaBufferIndex + 1) % TEMP_DELTA_BUFFER_SIZE;
+}
+
+void updateTargetTempDeltaBuffer(float delta)
+{
+    // Store the temperature delta in the buffer
+    temperatureTargetDeltaBuffer[currentTempTargetDeltaBufferIndex] = delta;
+    // Increment the index, wrapping around to the start of the buffer if necessary
+    currentTempTargetDeltaBufferIndex = (currentTempTargetDeltaBufferIndex + 1) % TEMP_DELTA_BUFFER_SIZE;
 }
 
 float calculateAverageTemp()
@@ -57,7 +94,7 @@ float calculateAverageTemp()
     // Calculate the average temperature from the buffer values
     float sum = 0;
     int count = 0;
-    for (int i = 0; i < BUFFER_SIZE; i++)
+    for (int i = 0; i < TEMP_BUFFER_SIZE; i++)
     {
         if (temperatureBuffer[i] != 0)
         {
@@ -68,49 +105,98 @@ float calculateAverageTemp()
     return sum / count;
 }
 
-
 void app_main()
 {
-    gpio_pad_select_gpio(LED_GPIO); 
+    gpio_pad_select_gpio(LED_GPIO);
     gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
-    gpio_pad_select_gpio(POWER_GPIO); 
-    gpio_set_direction(POWER_GPIO, GPIO_MODE_OUTPUT);
-    // gpio_pad_select_gpio(36); 
+    gpio_pad_select_gpio(HEATER_GPIO);
+    gpio_set_direction(HEATER_GPIO, GPIO_MODE_OUTPUT);
+
+    // gpio_pad_select_gpio(36);
     // gpio_set_direction(36, GPIO_MODE_INPUT);
+    float last_temp = 0;
 
     while (true)
     {
-        gpio_set_level(LED_GPIO, 1);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-        gpio_set_level(LED_GPIO, 0);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
 
+        // gpio_set_level(LED_GPIO, 1);
+        // vTaskDelay(100 / portTICK_PERIOD_MS);
+        // gpio_set_level(LED_GPIO, 0);
+        // vTaskDelay(100 / portTICK_PERIOD_MS);
 
+        // -------------------------------------------------------- AGGREGATE DATA FOR CONTROLLING
+
+        // Read the temperature
         float temp = readTemperature();
-
-        // Calculate the difference from the current temperature to the last measured temperature
-        float diff = 0;
-        if (currentTempBufferIndex > 0)
-        {
-            diff = temp - temperatureBuffer[currentTempBufferIndex - 1];
-        }
-
-        // Update the temperature delta buffer
-        temperatureDeltaBuffer[currentTempDeltaBufferIndex] = diff;
-        currentTempDeltaBufferIndex = (currentTempDeltaBufferIndex + 1) % TEMP_DELTA_BUFFER_SIZE;
-
         updateTempBuffer(temp);
 
+        float average_temp = calculateAverageTemp(); // Calculate the average temperature to flatten mini temperature spikes
+        updateAverageTempBuffer(average_temp);
+
+        // Calculate delta from the current temperature to the last measured temperature
+        float temp_delta = average_temp - last_temp;
+        last_temp = average_temp;
+        updateTempDeltaBuffer(temp_delta);
+
+        // Calculate Temperature Trend from average temperature delta over 1s
+        float one_second_old_last_av_temp = averageTemperatureBuffer[9];
+        float one_s_trend = average_temp - one_second_old_last_av_temp;
+
+        // Update the temperature-target delta buffer
+        float target_delta = TARGET_TEMP - average_temp;
+        updateTargetTempDeltaBuffer(target_delta);
+        float one_s_old_target_delta = temperatureTargetDeltaBuffer[9];
+        float one_s_target_delta_trend = target_delta - one_s_old_target_delta;
 
 
-        // vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // -------------------------------------------------------- CONTROL LOGIC
+        if (target_delta > 0.5) // action required
+        {
+        }
+        else // NO action required, target within bounds
+        {
+            continue;
+        }
 
-        float averageTemp = calculateAverageTemp();
+        // -------------------------------------------------------- CONTROL HEATER
+        float cycle = control_cycle / 100;
+        if (correction_power <= cycle)
+        {
+            // power on
+            gpio_set_level(HEATER_GPIO, 1);
+        }
+        else
+        {
+            // power off
+            gpio_set_level(HEATER_GPIO, 0);
+        }
 
-        // printf("Temp: %f\n", temp);
-        printf("Diff: %f\n", diff);
-        // printf("Average Temp: %f\n", averageTemp);
+        printf(" | Temp: %f", temp);
+        if(one_s_trend >= 0){
+            printf(" | one_s_trend:  %f", one_s_trend);
+        }else{
+            printf(" | one_s_trend: %f", one_s_trend);
+        }
 
-    
+        if(one_s_target_delta_trend >= 0){
+            printf(" | one_s_target_delta_trend:  %f", one_s_target_delta_trend);
+        }else{
+            printf(" | one_s_target_delta_trend: %f", one_s_target_delta_trend);
+        }
+        printf(" | Target Delta: %f", target_delta);
+        // printf(" | Correction power: %f", correction_power);
+
+        printf("\n");
+
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+
+        if (control_cycle == 100)
+        {
+            control_cycle = 0;
+        }
+        else
+        {
+            control_cycle++;
+        }
     }
 }
